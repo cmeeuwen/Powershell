fdf3d0bf46ee37bdaba147dd4aec56fae67be288
cmeeuwen <49039228+cmeeuwen@users.noreply.github.com>
2019-10-28 09:44:14 -0700

    Add files via upload

---
+++ MassEnableRemoteMailboxesEMC.ps1
@@ -0,0 +1,13 @@
+﻿Function Enableonprem{
+
+$PeopleToEnable = 'C:\SuperscriptProd\Test\UsernamesNeedEnabled.csv'
+
+Import-Csv $PeopleToEnable | ForEach {
+
+$User = $_.Username
+
+Enable-RemoteMailbox -Identity $User -RemoteRoutingAddress ($User + '@callruby.com') -Verbose
+
+}
+}
+Enableonprem
