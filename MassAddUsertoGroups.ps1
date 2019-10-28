79ac41fb8504980c742c29bc0a9216192e4fe889
cmeeuwen <49039228+cmeeuwen@users.noreply.github.com>
2019-10-28 09:45:02 -0700

    Add files via upload

---
+++ MassAddUsertoGroups.ps1
@@ -0,0 +1,76 @@
+﻿Function AddUserToMultipleGroups {
+
+                $MassAddUser = Read-Host "What user "
+
+                Add-Type -AssemblyName System.Windows.Forms
+                Add-Type -AssemblyName System.Drawing
+
+                
+                $form = New-Object System.Windows.Forms.Form
+                $form.Text = 'Security Groups and distro memberships'
+                $form.Size = New-Object System.Drawing.Size(400,300)
+                $form.StartPosition = 'CenterScreen'
+                
+
+                $OKButton = New-Object System.Windows.Forms.Button
+                $OKButton.Location = New-Object System.Drawing.Point(120,220)
+                $OKButton.Size = New-Object System.Drawing.Size(75,23)
+                $OKButton.Text = 'OK'
+                $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+                $form.AcceptButton = $OKButton
+                $form.AutoSize = $true
+                $form.Controls.Add($OKButton)
+
+                $CancelButton = New-Object System.Windows.Forms.Button
+                $CancelButton.Location = New-Object System.Drawing.Point(200,220)
+                $CancelButton.Size = New-Object System.Drawing.Size(75,23)
+                $CancelButton.Text = 'Cancel'
+                $CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+                $form.CancelButton = $CancelButton
+                $form.Controls.Add($CancelButton)
+
+                $label = New-Object System.Windows.Forms.Label
+                $label.Location = New-Object System.Drawing.Point(10,20)
+                $label.Size = New-Object System.Drawing.Size(280,20)
+                $label.Text = 'Please select groups to add user too:'
+                $form.Controls.Add($label)
+
+                $listBox = New-Object System.Windows.Forms.Listbox
+                $listBox.Location = New-Object System.Drawing.Point(15,60)
+                $listBox.Size = New-Object System.Drawing.Size(350,500)
+                
+
+                $listBox.SelectionMode = 'MultiSimple'
+
+
+                $getgroups = Get-ADGroup -SearchBase 'OU=Ruby Groups,DC=ruby,DC=local' -Filter 'ObjectClass -eq "group"' | select Name | Sort-Object -Property Name
+                
+
+                foreach ($group in $getgroups) {
+                                                [void] $listBox.Items.Add($group.name)
+                                                }
+                
+
+                $listBox.Height = 150
+                $form.Controls.Add($listBox)
+                $form.Topmost = $true
+
+                $result = $form.ShowDialog()
+
+                     if ($result -eq [System.Windows.Forms.DialogResult]::OK) {"True"} else {"Something went wrong!"}
+                     ForEach-Object {
+    
+                                        foreach ($group in $listBox.SelectedItems)
+                                        {
+                                            try{
+                                                Add-ADGroupMember -Identity $group $MassAddUser -Verbose
+                                                }
+                                                catch {
+                                                       Write-Host "$group does not exist! $MassAddUser not added!" -ForegroundColor Red
+                                                       }
+                                        
+                                        }
+        
+                                    }
+
+}
