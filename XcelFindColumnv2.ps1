a6d1c3bff982b9ac7e1c74f0a34b1b571c74347b
cmeeuwen <49039228+cmeeuwen@users.noreply.github.com>
2020-06-27 16:55:46 -0700

    Update XcelFindColumnv2.ps1

--- XcelFindColumnv2.ps1
+++ XcelFindColumnv2.ps1
@@ -1,5 +1,18 @@
 clear
 
+Function CleanUP {
+
+$XLOpenWB.close()
+$xlDotNet.Quit()
+$null = [System.Runtime.InteropServices.Marshal]::ReleaseComObject($XLOpenWS)
+$null = [System.Runtime.InteropServices.Marshal]::ReleaseComObject($XLOpenWB)
+$null = [System.Runtime.InteropServices.Marshal]::ReleaseComObject($xlDotNet)
+[System.GC]::Collect()
+[System.GC]::WaitForPendingFinalizers()
+$xlDotNet = $null
+
+}
+
 $erroractionpreference = "silentlycontinue"
 
 # Load Excel Comobject
@@ -16,6 +29,7 @@ $XLXSFile = "C:\SuperscriptProd\OnboardTemplates\CMO.csv"
 $XLOpenWB =  $xlDotNet.Workbooks.Open($XLXSFile)
 $XLOpenWS = $xlDotNet.Worksheets.Item("CMO")
 
+
 # Generates an array containing the Alphabetical Characters for column reference
 #===============================================================================#
 $ColumnletterArray = New-Object System.Collections.ArrayList
@@ -25,13 +39,19 @@ for ($num = 0; $num -lt 26; $num++)
     [char](65 + $num) | ForEach-Object {$ColumnletterArray.Add($_) | Out-Null}
 }
 
-# Generates Cell count into an array
-#===================================#
+
+# Generates Array Object for cell count
+#======================================#
 $CellCountArray = New-Object System.Collections.ArrayList
 
+
+# Stores Cell Count into variable
+#================================#
 $xlCellCount = [int32]$XLOpenWS.UsedRange.Cells.Count
 $xlCellCount++
-                                                         
+
+# Parses out Cell count out and adds to array
+#============================================#                       
 Do {
 
     $xlCellCount-- | Out-Null
@@ -45,7 +65,8 @@ Do {
                                                          
            $Column | Out-Null
 
-           # For Each Loop to store cell values into a variable
+           # For Each Loop to look up cell values 
+           #=====================================#
            ForEach ($Cell in $CellCountArray) {
                                                                                              
                    $findCellValue = $XLOpenWS.Range("$($Column)$($Cell)").Text
@@ -53,14 +74,17 @@ Do {
                    $ColumnCell = $Column + $Cell
                                                       
                    # If cell value contains specific string and cell number is D14 output to host
-                   If ($findCellValue -eq "Operations" -and $ColumnCell -eq "D14") {
+                   If ($findCellValue -eq "Operations" -and ($ColumnCell -eq "D14" -or $ColumnCell -eq "L14")) {
                                                                                                                                    
                       Write-Host "Cell: $($Column)$($Cell) contains the value of $($findCellValue)!" -ForegroundColor Green
                                                          
                                                                                     #Insert code here
                                                                                     #================
-                                                                                                                                   
-                                                                                                                                   
+                                                                                    
+                      Write-Host "Replacing value in cell $($ColumnCell) with Test"                                               
+                      [void]$XLOpenWS.Range("$ColumnCell", "$ColumnCell").Replace("Operations", "Test", [Microsoft.Office.Interop.Excel.XlLookAt]::xlPart)
+                                                                                    
+                      $XLOpenWB.save()                                             
                                                                                            
                                                                                            
                                                                                            
@@ -82,8 +106,8 @@ Do {
                                                                                            
                                                                                            
                                                                                            
-                                                                                           
-                                                                                           
+                                                                                   CleanUP        
+                                                                                   Break Script        
                                                                                    }
 
                                                                                                                                    
@@ -98,12 +122,4 @@ Do {
           
                                             }
 
-
-$XLOpenWB.close()
-$xlDotNet.Quit()
-$null = [System.Runtime.InteropServices.Marshal]::ReleaseComObject($XLOpenWS)
-$null = [System.Runtime.InteropServices.Marshal]::ReleaseComObject($XLOpenWB)
-$null = [System.Runtime.InteropServices.Marshal]::ReleaseComObject($xlDotNet)
-[System.GC]::Collect()
-[System.GC]::WaitForPendingFinalizers()
-$xlDotNet = $null
+CleanUP
